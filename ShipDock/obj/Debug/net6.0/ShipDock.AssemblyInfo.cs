4ee61d092f93a8cbc6765891fccbbc7da8603226
Ignaselis <95550506+shiver225@users.noreply.github.com>
2024-04-28 06:32:28 +0300

    fastest

--- ShipDock/obj/Debug/net6.0/ShipDock.AssemblyInfo.cs
+++ ShipDock/obj/Debug/net6.0/ShipDock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShipDock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa73f8a7d1b30ff8bfa2ed5ae75a7cfc5532599e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a5bb4936596bb2f435c0f00c04638596c0f7d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShipDock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShipDock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
