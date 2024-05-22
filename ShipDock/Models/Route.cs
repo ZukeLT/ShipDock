f0a1bfd18a541cbc66195c5aa2a2a2673200c5ff
Rokas Stadalius <dstadaalius20@gmail.com>
2024-05-22 22:28:13 +0300

    sulauziau appsa :3

---
+++ ShipDock/Models/Route.cs
@@ -0,0 +1,17 @@
+﻿namespace ShipDock.Models
+{
+    public class Route
+    {
+        public int RouteID { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Distance { get; set; }
+
+        public Route(int routeID, string from, string to, int distance) {
+            this.RouteID = routeID;
+            this.From = from;
+            this.To = to;
+            this.Distance = distance;
+        }
+    }
+}
---
+++ ShipDock/Models/Schedule.cs
@@ -0,0 +1,25 @@
+﻿using ShipDock.Helper;
+
+namespace ShipDock.Models
+{
+	public class Schedule
+	{
+		public DateTime? LeaveDate { get; set; }
+		public DateTime? ArriveDate { get; set; }
+		public int ShipID { get; set; }
+
+		public int TripID { get; set; }
+		public int RouteID { get; set; }
+		public int DockID { get; set; }
+		public string ShipModel { get; set; }
+		public string From { get; set; }
+		public string To { get; set; }
+		public string DepartureStatus { get; set; }
+
+		public bool Insert()
+		{
+			return DataSource.UpdateDataSQL($"INSERT INTO [dbo].[Trip] ([LeaveDate],[ArriveDate],[RouteID],[DockID],[ShipID])"
+				+ $"VALUES ('{LeaveDate}', '{ArriveDate}', '{RouteID}', '{DockID}', '{ShipID}')");
+		}
+	}
+}
